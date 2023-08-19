fd53cc255e2e6fc25309dbce1a7c40fd7427b2ba
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-19 10:21:52 +0330

    Add files via upload

---
+++ ConsoleApp97/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 5;
+            Console.WriteLine($"c before postincrement:{c}");
+            Console.WriteLine($"postincrementing c:{c++}");
+            Console.WriteLine($"c after postincrement:{c}");
+
+            c = 5;
+            Console.WriteLine($"c before postincrement:{c}");
+            Console.WriteLine($"postincrementing c:{++c}");
+            Console.WriteLine($"c after postincrement:{c}");
+            Console.ReadLine();
+        }
+    }
+}
